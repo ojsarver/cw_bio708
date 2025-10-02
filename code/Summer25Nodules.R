753e51a67a48ed97ee5f32543712dadc982a1689
ojsarver <ojsarver@uncg.edu>
2025-10-02 10:26:48 -0400

    summer25nodulesanalysis

---
+++ code/Summer25Nodules.R
@@ -0,0 +1,39 @@
+
+# t.test ------------------------------------------------------------------
+
+library(pacman)
+pacman::p_load(tidyverse,
+               patchwork,
+               here,
+               ggplot2)
+
+df_nod <- read_csv(here("data_raw/Summer25_Nodules.csv"))
+
+x <- df_nod %>%
+  filter(Treatment == "C") %>%
+  pull(Nodules)
+
+y <- df_nod %>%
+  filter(Treatment == "MP") %>%
+  pull(Nodules)
+
+t.test(x, y, var.equal = FALSE)
+
+
+# chart -------------------------------------------------------------------
+
+df_nod_mu <- df_nod %>% 
+  group_by(Treatment) %>% # group operation
+  summarize(mu_n = mean(Nodules),
+            sd_n = sd(Nodules))
+
+Legend=c("Control","Microplastic")
+
+df_nod_mu %>%
+  ggplot(aes(x=Treatment,
+             y=mu_n, fill=Legend))+
+  geom_bar(stat="identity")+
+  scale_fill_manual("Legend", values = c("Control" = "lightblue2",
+                                         "Microplastic" = "green4"))+
+  labs(x = "Treatment",
+       y = "Average Number of Nodules")
