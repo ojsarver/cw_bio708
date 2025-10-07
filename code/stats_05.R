85805e00b4fc36e6d460abe08b4e350c0a2b8435
ojsarver <ojsarver@uncg.edu>
2025-10-07 10:37:44 -0400

    stats_05

---
+++ code/stats_05.R
@@ -0,0 +1,89 @@
+
+library(pacman)
+pacman::p_load(tidyverse,
+               patchwork,
+               here)
+
+# read data ---------------------------------------------------------------
+
+df_anova<-read_csv(here("data_raw/data_fish_length_anova.csv"))
+
+#get unnique elements
+colnames(df_anova)
+distinct(df_anova,lake)
+
+#visuals
+df_anova%>%
+  ggplot(aes(x=lake,
+             y=length))+
+  geom_violin(draw_quantiles=.5,
+              alpha=.2)+
+  geom_jitter(width=.1)+
+  theme_bw()
+
+
+# anova -------------------------------------------------------------------
+
+#anova function not ideal because issues doing type 1 or type 2
+#response variable (y) should be on left, predictor variable (x) on left
+#use aov function for anova
+
+m<-aov(length~lake,
+    data=df_anova)
+
+#summary to get more details, Pr(>F) is p-value
+summary(m)
+
+
+# between group variability -----------------------------------------------
+
+
+mu<-mean(df_anova$length)
+
+
+s_b<- df_anova%>%
+  group_by(lake)%>%
+  summarize(mu_g=mean(length),
+            n=n())%>%
+  mutate(dev_g=(mu_g-mu)^2,
+         ss_g=dev_g*n)%>%
+  pull(ss_g)%>%
+  sum()
+
+# within group variability ------------------------------------------------
+
+s_w<-df_anova%>%
+  group_by(lake)%>%
+  mutate(mu_g=mean(length))%>%
+  ungroup()%>%
+  mutate(dev_i=(length-mu_g)^2)%>%
+  pull(dev_i)%>%
+  sum()
+
+
+# summed square to variance -----------------------------------------------
+
+
+s2_b<-s_b/(n_distinct(df_anova$lake)-1)
+s2_w<-s_w/(nrow(df_anova)-n_distinct(df_anova$lake)-1)
+
+# calculate f stat --------------------------------------------------------
+
+
+f_value<-s2_b/s2_w
+f_null<-seq(0,10,by=.1)
+y<-df(x=f_null,
+      df1=2,
+      df2=147)
+
+tibble(x=f_null,
+       y=y)%>%
+  ggplot(aes(x=x,
+             y=y))+
+  geom_line()+
+  geom_vline(xintercept = f_value,
+             color="pink")
+
+1-pf(q=f_value,
+   df1=2,
+   df2=147)
